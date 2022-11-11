db06e4f6c864a03cb8dd2dcd0b071c6769822a77
rmadbstjd <rmadbstjd@naver.com>
2022-11-11 19:19:29 +0900

    슬라이드 기능을 위한 라이브러리 설치 및 세팅

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React,{useEffect} from 'react';
 import { movieAction } from './../redux/actions/movieAction';
 import {useDispatch, useSelector} from 'react-redux';
 import Banner from '../components/Banner';
+import MovieSlide from '../components/MovieSlide';
 const Main = () => {
     const {popularMovies, topRatedMovies, upComingMovies} = useSelector((state) => state.movie);
     
@@ -12,7 +13,12 @@ const Main = () => {
     return (
         <div>
             {popularMovies.results && <Banner movie={popularMovies.results[0]}/>}
-          
+            <h1>Popular Movies</h1>
+            <MovieSlide movies={popularMovies}></MovieSlide>
+            <h1>Top Rated Movies</h1>
+            <MovieSlide movies={topRatedMovies}></MovieSlide>
+            <h1>UpComing Movies</h1>
+            <MovieSlide movies={upComingMovies}></MovieSlide>
         </div>
     );
 };
